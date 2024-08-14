7213617dfd6a04241f80e0ca2f19dca48e9625ec
Elissa-2003 <elissaboni@gmail.com>
2024-08-14 18:41:54 -0300

    Agregar evento al div

---
+++ eventos.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded" , ()=> { 
+
+    document.getElementsByTagName("div").addEventListener("click", ()=> {
+        alert ("Hola! Soy el div") // al clickear el div nos saltara una alerta con un saludo
+    });
+});
---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded" , ()=> { 
+
+    document.getElementById("boton").addEventListener("click", ()=> {
+        alert ("Hola") // al clickear el boton nos saltara una alerta con un saludo
+    });
+});
